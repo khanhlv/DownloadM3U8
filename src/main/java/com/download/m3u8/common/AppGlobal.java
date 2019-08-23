8ae021b72af56373ba01fe2b5c89ce7cde69be44
khanhlv5 <khanhlv5@vpbank.com.vn>
2019-08-23 15:03:20 +0700

    update

--- src/main/java/com/download/m3u8/common/AppGlobal.java
+++ src/main/java/com/download/m3u8/common/AppGlobal.java
@@ -6,6 +6,21 @@ public class AppGlobal {
 	public final static String USER_NAME_1 = "study.mkt@gmail.com";
 	public final static String PASSWORD_1 = "tambooks@mkt2019";
 
-	public final static String USER_NAME_2 = "study.uvn@gmail.com";
-	public final static String PASSWORD_2 = "tambooks@quanly2019";
+	public final static String USER_NAME_2 = "study.nuoidaycon@gmail.com";
+	public final static String PASSWORD_2 = "tambooks@nuoidaycon2019";
+
+	public final static String USER_NAME_3 = "study.phongthe@gmail.com";
+	public final static String PASSWORD_3 = "tambooks@phongthe2019";
+
+	public final static String USER_NAME_4 = "study.thietke@gmail.com";
+	public final static String PASSWORD_4 = "tambooks@thietke2019";
+
+	public final static String USER_NAME_5 = "study.tinhyeu@gmail.com";
+	public final static String PASSWORD_5 = "tambooks@tinhyeu2019";
+
+	public final static String USER_NAME_6 = "study.uvn@gmail.com";
+	public final static String PASSWORD_6 = "tambooks@quanly2019";
+
+	public final static String USER_NAME_7 = "study.yoga@gmail.com";
+	public final static String PASSWORD_7 = "tambooks@yoga2019";
 }
